8ff1bc8c7b17a8c94ef3ba918e5a5c29c9e97d51
bdbrown12 <123220632+bdbrown12@users.noreply.github.com>
2025-01-19 23:08:32 -0700

    Started creating the logic and methods for the calculator

---
+++ calculator.js
@@ -0,0 +1,25 @@
+const num1 = 0;
+const num2 = 0;
+let operator = [add, subtract, multiply, divide];
+
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    
+}
+
+function divide(a,b) {
+
+}
+
+function operate(num1, num2, operator) {
+    
+}
+
